b5490ecdea5498d9aacf4ebf1578e8a1e68f2d08
youssef elgharieb <youssef.elgharieb@gmail.com>
2024-10-08 14:13:21 +0300

    user-seed

--- Mates.Infrastructure/Configuration/UserEntityTypeConfiguration.cs
+++ Mates.Infrastructure/Configuration/UserEntityTypeConfiguration.cs
@@ -9,11 +9,6 @@ namespace Mates.Infrastructure.Configuration
 {
     public class UserEntityTypeConfiguration : IEntityTypeConfiguration<User>
     {
-        IPasswordService _passwordService;
-        public UserEntityTypeConfiguration(IPasswordService passwordService) 
-        {
-            _passwordService = passwordService ?? throw new ArgumentException(nameof(passwordService));
-        }
 
         public void Configure(EntityTypeBuilder<User> builder)
         {
@@ -21,15 +16,15 @@ namespace Mates.Infrastructure.Configuration
                 .Property(u => u.Role)
                 .HasDefaultValue(Role.User);
 
-            var hashedPassword = _passwordService.Hash("8&xGnGpCASlo$3q");
-
+            //m.ha@luftborn.com's password: 
+            //8&xGnGpCASlo$3q
             builder
                 .HasData(new User()
                 {
                     Id = Guid.NewGuid(),
-                    Email = "marwan@Luftborn",
-                    Password = hashedPassword,
-                    Name = "marwan",
+                    Email = "m.ha@luftborn.com",
+                    Password = "$2a$11$PRYh/ESkoB7jNbAsl0z3HefeGscGxvBau7ZL0UBN/FBPuWft7ZC7a",
+                    Name = "marwan hamed",
                     Role = Role.Admin
                 });
         }
